bdf10493c6422a1fb42ff5dd0ef901cdc65d7f46
ihorbar <ihorbar1@gmail.com>
2018-10-27 12:52:46 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $(".owl-carousel").owlCarousel({
+     items: 1,
+     dots: true,
+    //  margin: 10
+   });
+ });
+
+ var test = document.getElementById("test");
+
+ console.dir(test)
